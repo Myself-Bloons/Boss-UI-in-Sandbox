1cd12b868f4aa93bed3acce63cbcaa63e22a46ef
Myself <38400384+Myself-Bloons@users.noreply.github.com>
2025-11-16 18:00:41 +0100

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,7 +4,7 @@ public static class ModHelperData
 {
     public const string Name = "BossUI in SandBox";
     public const string Description = "Adds BossUI, Health bar, Tiers, Skulls, etc... and all Boss Bloons (Bloonarius, Vortex, Lych, Dreadbloon, Phayze + Elite), golden blons, and Dreadbloon rocks to SandBox mode.";
-    public const string Version = "1.1.0";
+    public const string Version = "2.0.0";
     public const string RepoOwner = "Myself-Bloons";
     public const string RepoName = "BossUIinSandbox";
     public const string Author = "Myself";
